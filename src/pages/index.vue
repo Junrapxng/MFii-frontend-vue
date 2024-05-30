834134d1fc6084afc5c5be6ce2c055784bd7534a
KEAMIKA KANTHAYUONG <6431501010@lamduan.mfu.ac.th>
2024-05-31 05:23:43 +0700

    index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,20 @@
 <template>
-  <test />
+  <v-app>
+    <NavBar />
+    <router-view></router-view>
+  </v-app>
 </template>
 
-<script setup>
-  //
+<script>
+// import NavBar from './components/NavBar.vue';
+
+// export default {
+//   components: {
+//     NavBar
+//   }
+// }
 </script>
+
+<style>
+
+</style>
