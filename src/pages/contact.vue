e8f84f17900097e4f5815331bacf4708d2eed913
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-05-31 21:54:35 +0700

    contact page

---
+++ src/pages/contact.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <NavBar></NavBar>
+    
+    <v-main>
+      <v-container>
+        
+        <v-row>
+          <v-col col="9" md="9" lg="9">
+            <div
+              id="c16127"
+              class="frame frame-default frame-type-header frame-layout-0"
+            >
+              <header>
+                <h2 class="">ติดต่อเรา</h2>
+              </header>
+            </div>
+
+            <div
+              id="c17557"
+              class="frame frame-default frame-type-text frame-layout-0"
+            >
+              <p>
+                <span style="font-size: 18px"
+                  >ส่วนจัดการทรัพย์สินทางปัญญาและนวัตกรรม
+                  มหาวิทยาลัยแม่ฟ้าหลวง<br />
+                  333 ม.1 ต.ท่าสุด อ.เมือง จ.เชียงราย 57100<br />
+                  โทรศัพท์. 0 5391 7003 และ 7011<br />
+                  อีเมล: mfii@mfu.ac.th</span
+                >
+              </p>
+            </div>
+
+            <div
+              id="c17556"
+              class="frame frame-default frame-type-html frame-layout-0"
+            >
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4109.340290300189!2d99.89215215071464!3d20.044947186473486!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30d700048c69def1%3A0xa20592e502bc20c9!2sMae%20Fah%20Luang%20University!5e1!3m2!1sen!2sth!4v1573545998005!5m2!1sen!2sth"
+                width="100%"
+                height="450"
+                frameborder="0"
+                style="border: 0"
+                allowfullscreen=""
+              ></iframe>
+            </div>
+          </v-col>
+          <!-- <v-col col="9" md="9" lg="9"> </v-col> -->
+          <v-col col="3" md="3"></v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <Footer></Footer>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  name: "contact-page",
+
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
