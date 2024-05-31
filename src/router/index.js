ae65b1aa4daea659c61b159294a17bd2130ffcfe
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-05-31 20:02:13 +0700

    add path

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,26 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../pages/index.vue'
+import Contact from '../pages/contact.vue'
+
+const routes = [
+    {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+]
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),routes
 })
 
 export default router
