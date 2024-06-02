70f18f51c2be92ee8d1b0a41aeadd323da0b4377
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-06-03 00:58:45 +0700

    Update index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,82 +1,109 @@
 <template>
-  <v-app>
+<v-app>
     <NavBar />
     <v-main>
-    <!-- Carousel Slide -->
-      <Carousel v-bind="settings" :autoplay="2000" :wrap-around="true">
-    <Slide v-for="slide in 10" :key="slide">
-      <div class="carousel__item">{{ slide }}</div>
-    </Slide>
+        <!-- Carousel Slide -->
+        <Carousel :autoplay="4000" :wrap-around="true">
+            <Slide v-for="img in images" :key="img.id">
+                <div class="carousel__item">
+                  <img :src="`${img.image}`" alt="test">
+                </div>
+            </Slide>
+            <template #addons>
+                <Pagination />
+                <Navigation />
+            </template>
+        </Carousel>
+        <!-- Carousel Slide -->
 
-    <template #addons>
-      <Pagination />
-      <Navigation />
-    </template>
-      </Carousel>
-    <!-- Carousel Slide -->
-
-    <!-- Content -->
-    <div>
-
-    </div>
+        <!-- Content -->
+        <div v-for="data in info">
+            {{ data.plant_name }}
+        </div>
 
     </v-main>
     <Footer></Footer>
     <router-view></router-view>
-  </v-app>
+</v-app>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-import { Carousel, Navigation, Slide, Pagination } from 'vue3-carousel'
+import {
+    defineComponent
+} from 'vue'
+import {
+    Carousel,
+    Navigation,
+    Slide,
+    Pagination
+} from 'vue3-carousel'
 import 'vue3-carousel/dist/carousel.css'
 import axios from 'axios';
+
 export default defineComponent({
-  name: 'index-page',
-  mounted(){
-    axios.get('https://65fb5ab714650eb21009db19.mockapi.io/plant')
-    .then(response => console.log(response))
-  },
-  components: {
-    Pagination,
-    Carousel,
-    Slide,
-    Navigation,
-  },
-  data: () => ({
-    // carousel settings
-    settings: {
-      itemsToShow: 1,
-      snapAlign: 'center',
+    name: 'index-page',
+    data() {
+        return {
+            info: [],
+            images: [],
+             // Define info property here
+        }
     },
-    // breakpoints are mobile first
-    // any settings not specified will fallback to the carousel settings
+    mounted() {
+      this.fetchData();
+    },
+    methods:{
+      fetchData() {
+      // Fetch data from both APIs
+      const api1Promise = axios.get('https://65fb5ab714650eb21009db19.mockapi.io/plant');
+      const api2Promise = axios.get('https://65fb5ab714650eb21009db19.mockapi.io/todos');
+
+      // Handle both responses simultaneously using Promise.all
+      Promise.all([api1Promise, api2Promise])
+        .then(([api1Response, api2Response]) => {
+          this.info = api1Response.data;
+          this.images = api2Response.data;
+        })
+        .catch(error => {
+          this.error = error; // Handle errors
+        });
+    },
+  },
+
+
+
+
+
+
+  
+    components: {
+        Pagination,
+        Carousel,
+        Slide,
+        Navigation,
+    },
+    // Remove the duplicate data declaration here
+    // settings and breakpoints can go outside of data
+    // settings: {
+    //     itemsToShow: 1,
+    //     snapAlign: 'center',
+    // },
     breakpoints: {
-      // 700px and up
-      700: {
-        itemsToShow: 3.5,
-        snapAlign: 'center',
-      },
-      // 1024 and up
-      1024: {
-        itemsToShow: 5,
-        snapAlign: 'start',
-      },
+        // 700px and up
+        700: {
+            itemsToShow: 3.5,
+            snapAlign: 'center',
+        },
+        // 1024 and up
+        1024: {
+            itemsToShow: 5,
+            snapAlign: 'start',
+        },
+
     },
-  }),
 })
 </script>
 
 <style>
-.carousel__item {
-  min-height: 400px;
-  width: 80%;
-  background-color: green;
-  color: var(--vc-clr-white);
-  font-size: 20px;
-  border-radius: 8px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+
 </style>
