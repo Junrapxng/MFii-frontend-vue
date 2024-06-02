5b140cebd8e37c56039242225a427311e076044d
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-06-02 21:03:45 +0700

    slider

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,24 @@
   <v-app>
     <NavBar />
     <v-main>
-      
+    <!-- Carousel Slide -->
+      <Carousel v-bind="settings" :autoplay="2000" :wrap-around="true">
+    <Slide v-for="slide in 10" :key="slide">
+      <div class="carousel__item">{{ slide }}</div>
+    </Slide>
+
+    <template #addons>
+      <Pagination />
+      <Navigation />
+    </template>
+      </Carousel>
+    <!-- Carousel Slide -->
+
+    <!-- Content -->
+    <div>
+
+    </div>
+
     </v-main>
     <Footer></Footer>
     <router-view></router-view>
@@ -10,11 +27,56 @@
 </template>
 
 <script>
-export default {
-  name: "index-page",
-};
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Slide, Pagination } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+import axios from 'axios';
+export default defineComponent({
+  name: 'index-page',
+  mounted(){
+    axios.get('https://65fb5ab714650eb21009db19.mockapi.io/plant')
+    .then(response => console.log(response))
+  },
+  components: {
+    Pagination,
+    Carousel,
+    Slide,
+    Navigation,
+  },
+  data: () => ({
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: 'center',
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      700: {
+        itemsToShow: 3.5,
+        snapAlign: 'center',
+      },
+      // 1024 and up
+      1024: {
+        itemsToShow: 5,
+        snapAlign: 'start',
+      },
+    },
+  }),
+})
 </script>
 
 <style>
-
+.carousel__item {
+  min-height: 400px;
+  width: 80%;
+  background-color: green;
+  color: var(--vc-clr-white);
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
