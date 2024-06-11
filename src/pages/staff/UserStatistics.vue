24a4d7d23d8d1af3653ba82b1a6600b4c041a1d8
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-06-11 22:40:54 +0700

    50%

--- src/pages/staff/UserStatistics.vue
+++ src/pages/staff/UserStatistics.vue
@@ -6,6 +6,24 @@
           <v-row>
             <v-col>
               <v-card>
+                <v-card-title>จำนวนผู้ใช้งานวันนี้</v-card-title>
+                <v-card-text>{{ todayUsers }}</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col>
+              <v-card>
+                <v-card-title>จำนวนผู้ใช้งานสัปหาห์นี้</v-card-title>
+                <v-card-text>{{ weeklyUsers }}</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col>
+              <v-card>
+                <v-card-title>จำนวนผู้ใช้งานเดือนนี้</v-card-title>
+                <v-card-text>{{ monthlyUsers }}</v-card-text>
+              </v-card>
+            </v-col>
+            <v-col>
+              <v-card>
                 <v-card-title>จำนวนผู้ใช้งานทั้งหมด</v-card-title>
                 <v-card-text>{{ totalUsers }}</v-card-text>
               </v-card>
@@ -21,6 +39,10 @@
     data() {
       return {
         totalUsers: 0,
+        todayUsers: 0,
+        weeklyUsers: 0,
+        monthlyUsers: 0,
+        
       }
     },
     created() {
