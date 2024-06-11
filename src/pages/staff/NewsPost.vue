a6d1eb3efa456552f28b09d595be7c60c6c3e298
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-06-11 21:53:45 +0700

    staff new post page

---
+++ src/pages/staff/NewsPost.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-main>
+      <staff-layout>
+        <v-container>
+          <v-card>
+            <v-card-title>สร้างโพสข่าวสาร</v-card-title>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  label="หัวข้อ"
+                  v-model="news.title"
+                ></v-text-field>
+                <v-textarea label="เนื้อหา" v-model="news.content"></v-textarea>
+                <v-btn @click="submitNews">บันทึก</v-btn>
+              </v-form>
+            </v-card-text>
+          </v-card>
+        </v-container>
+      </staff-layout>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  import StaffLayout from "@/layouts/staff.vue";
+export default {
+  name: "staff-newPost-page",
+    components: {
+        StaffLayout,
+  },
+  data() {
+    return {
+      news: {
+        title: "",
+        content: "",
+      },
+    };
+  },
+  methods: {
+    submitNews() {
+      // logic to submit news
+    },
+  },
+};
+</script>
+
+<style scoped></style>
