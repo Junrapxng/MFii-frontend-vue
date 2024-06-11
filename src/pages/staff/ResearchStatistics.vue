d7cc4ec44507cb08037ffc47954ce76c3e4acbcb
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-06-11 21:54:42 +0700

    research view page

---
+++ src/pages/staff/ResearchStatistics.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container>
+      <v-card>
+        <v-card-title>สถิติผลงานวิจัยที่ถูกเข้าถึงบ่อย</v-card-title>
+        <v-card-text>
+          <v-data-table :headers="headers" :items="popularResearches" class="elevation-1">
+          </v-data-table>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        headers: [
+          { text: 'ชื่อผลงาน', value: 'title' },
+          { text: 'จำนวนการเข้าชม', value: 'views' }
+        ],
+        popularResearches: []
+      }
+    },
+    created() {
+      this.fetchResearchStatistics();
+    },
+    methods: {
+      fetchResearchStatistics() {
+        // logic to fetch research statistics
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
