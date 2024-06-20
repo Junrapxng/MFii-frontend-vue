8fc80f60a21a8be102efe87aa98af26daeb40927
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-06-20 07:54:21 +0700

    update function manage users

--- src/pages/admin/userManage.vue
+++ src/pages/admin/userManage.vue
@@ -4,152 +4,100 @@
   <admin-layout>
     <!-- Content for admin page goes here -->
     <v-container>
-       <!-- Topic -->
+      <!-- Topic -->
       <v-card class="rounded-xl pa-4">
         <v-card-title class="d-flex align-center pe-2">
-          <v-icon icon="mdi-account-group"></v-icon> &nbsp; User Management 
+          <v-icon icon="mdi-account-group"></v-icon> &nbsp; User Management
 
           <v-spacer></v-spacer>
 
           <!-- search -->
-          <v-text-field
-            v-model="search"
-            density="compact"
-            label="Search"
-            prepend-inner-icon="mdi-magnify"
-            variant="solo-filled"
-            flat
-            clearable
-            hide-details
-            single-line
-          ></v-text-field>
+          <v-text-field v-model="search" density="compact" label="Search" prepend-inner-icon="mdi-magnify"
+            variant="solo-filled" flat clearable hide-details single-line></v-text-field>
 
           <!-- Add User -->
-           <!-- <template v-slot:top> -->
-            <!-- <v-toolbar flat> -->
-              <!-- <v-toolbar-title>My CRUD</v-toolbar-title> -->
-              <!-- <v-divider class="mx-4" inset vertical></v-divider> -->
-              <!-- <v-spacer></v-spacer> -->
-              <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ props }">
-                  <v-btn class="mx-4" color="primary" dark v-bind="props">
-                   <v-icon class="pr-2">mdi-plus</v-icon> New User
-                  </v-btn>
-                </template>
-                <v-card class="rounded-xl pa-4">
-                  <v-card-title>
-                    <span class="text-h5">{{ formTitle }}</span>
-                  </v-card-title>
+          <!-- <template v-slot:top> -->
+          <!-- <v-toolbar flat> -->
+          <!-- <v-toolbar-title>My CRUD</v-toolbar-title> -->
+          <!-- <v-divider class="mx-4" inset vertical></v-divider> -->
+          <!-- <v-spacer></v-spacer> -->
+          <v-dialog v-model="dialog" max-width="500px">
+            <template v-slot:activator="{ props }">
+              <v-btn class="mx-4" color="primary" dark v-bind="props">
+                <v-icon class="pr-2">mdi-plus</v-icon> New User
+              </v-btn>
+            </template>
+            <v-card class="rounded-xl pa-4">
+              <v-card-title>
+                <span class="text-h5">{{ formTitle }}</span>
+              </v-card-title>
 
-                  <v-card-text>
-                    <v-container>
-                      <v-row>
-                        <v-col cols="12" md="12" sm="12">
-                          <v-text-field
-                          variant="solo-filled"
-                            flat
-                            v-model="editedItem.name"
-                            label="Name"
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" md="12" sm="12">
-                          <v-text-field
-                          variant="solo-filled"
-                            flat
-                            v-model="editedItem.email"
-                            label="Email"
-                            :rules="[
-                              (v) => !!v || 'กรุณากรอก อีเมล',
-                              (v) => /.+@.+\..+/.test(v) || 'กรุณากรอกอีเมลให้ถูกต้อง',
-                            ]"
-                            required
-                          ></v-text-field>
-                        </v-col>
-                        <v-col cols="12" md="6" sm="6">
-                          <v-autocomplete
-                          variant="solo-filled"
-                            flat
-                            label="Role"
-                            v-model="editedItem.role"
-                            :items="['Staff', 'Admin']"
-                          ></v-autocomplete>
-                        </v-col>
-                        <v-col cols="12" md="6" sm="6">
-                          <v-autocomplete
-                          variant="solo-filled"
-                            flat
-                            label="Status"
-                            v-model="editedItem.status"
-                            :items="['Active', 'Inactive']"
-                          ></v-autocomplete>
-                        </v-col>                      
-                      </v-row>
-                    </v-container>
-                  </v-card-text>
+              <v-card-text>
+                <v-container>
+                  <v-row>
+                    <v-col cols="12" md="12" sm="12">
+                      <v-text-field variant="solo-filled" flat v-model="editedItem.name" label="Name"></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="12" sm="12">
+                      <v-text-field variant="solo-filled" flat v-model="editedItem.email" label="Email" :rules="[
+                        (v) => !!v || 'กรุณากรอก อีเมล',
+                        (v) => /.+@.+\..+/.test(v) || 'กรุณากรอกอีเมลให้ถูกต้อง',
+                      ]" required></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6" sm="6">
+                      <v-autocomplete variant="solo-filled" flat label="Role" v-model="editedItem.role"
+                        :items="['staff', 'admin']"></v-autocomplete>
+                    </v-col>
+                    <v-col cols="12" md="6" sm="6">
+                      <v-autocomplete variant="solo-filled" flat label="Status" v-model="editedItem.status"
+                        :items="['Active', 'Inactive']"></v-autocomplete>
+                    </v-col>
+                  </v-row>
+                </v-container>
+              </v-card-text>
 
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="red-darken-1" variant="outlined" class="hover:bg-red-300" @click="close">
-                      Cancel
-                    </v-btn>
-                    <v-btn color="green-darken-1" variant="outlined" class=" hover:bg-green-200" @click="save">
-                      Save
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="red-darken-1" variant="outlined" class="hover:bg-red-300" @click="close">
+                  Cancel
+                </v-btn>
+                <v-btn color="green-darken-1" variant="outlined" class=" hover:bg-green-200" @click="save">
+                  Save
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
 
-              <!-- Delete User -->
-              <v-dialog v-model="dialogDelete" max-width="600px">
-                <v-card class="rounded-xl pa-4">
-                  <v-card-title class="text-h5 text-center text-red-500"
-                    >Are you sure you want to delete this user?</v-card-title
-                  >
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                      color="'blue-grey-darken-1"
-                      variant="outlined"
-                      class="hover:bg-gray-500"
-                      @click="closeDelete"
-                      >Cancel</v-btn
-                    >
-                    <v-btn
-                      color="red-darken-1"
-                      variant="outlined"
-                      class="hover:bg-red-300"
-                      @click="deleteItemConfirm"
-                      >OK</v-btn
-                    >
-                    <v-spacer></v-spacer>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
-            <!-- </v-toolbar> -->
+          <!-- Delete User -->
+          <v-dialog v-model="dialogDelete" max-width="600px">
+            <v-card class="rounded-xl pa-4">
+              <v-card-title class="text-h5 text-center text-red-500">Are you sure you want to delete this
+                user?</v-card-title>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="'blue-grey-darken-1" variant="outlined" class="hover:bg-gray-500"
+                  @click="closeDelete">Cancel</v-btn>
+                <v-btn color="red-darken-1" variant="outlined" class="hover:bg-red-300"
+                  @click="deleteItemConfirm">OK</v-btn>
+                <v-spacer></v-spacer>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <!-- </v-toolbar> -->
           <!-- </template> -->
 
-          
+
         </v-card-title>
         <v-divider></v-divider>
 
-         <!-- data table -->
-        <v-data-table
-        v-model:search="search" 
-          :headers="headers"
-          :items="users.resutl"
-          :items-per-page="-1"
-          :sort-by="[{ key: 'userId', order: 'asc' }]"
-          :fixed-header="true"
-          height="400"
-        >
+        <!-- data table -->
+        <v-data-table v-model:search="search" :headers="headers" :items="users.resutl" :items-per-page="-1"
+          :sort-by="[{ key: 'userId', order: 'asc' }]" :fixed-header="true" height="400">
 
           <template v-slot:item.status="{ item }">
             <div>
-              <v-chip
-                :color="item.status.toLowerCase() === 'active' ? 'green' : 'red'"
-                class="text-uppercase"
-                size="small"
-                label>
+              <v-chip :color="item.status.toLowerCase() === 'active' ? 'green' : 'red'" class="text-uppercase"
+                size="small" label>
                 {{ item.status }}
               </v-chip>
             </div>
@@ -170,6 +118,51 @@
       </v-card>
     </v-container>
   </admin-layout>
+
+<!-- Snackbar Delete -->
+  <div class="text-center">
+    <v-snackbar
+      v-model="snackbardel"
+      vertical
+    >
+      <div class="text-subtitle-1 pb-2">Success</div>
+
+      <p>Deleted Success</p>
+
+      <template v-slot:actions>
+        <v-btn
+          color="indigo"
+          variant="text"
+          @click="snackbardel = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+<!-- Snackbar Edited -->
+  <div class="text-center" >
+    <v-snackbar
+      v-model="snackbaredit"
+      vertical
+    >
+      <div class="text-subtitle-1 pb-2">Success</div>
+
+      <p>Edited Success</p>
+
+      <template v-slot:actions>
+        <v-btn
+          color="indigo"
+          variant="text"
+          @click="snackbaredit = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+
+
 </template>
 
 <script>
@@ -182,15 +175,17 @@ export default {
   data: () => ({
     search: '',
     users: [],
+    snackbardel: false,
+    snackbaredit: false,
     dialog: false,
     dialogDelete: false,
     headers: [
-      { title: 'ID',align: 'center',key: '_id',},
-      { title: "Name",align: "start",sortable: false,key: "name",},
-      { title: "Email",sortable: false, key: "email" },
+      { title: 'ID', align: 'center', key: '_id', },
+      { title: "Name", align: "start", sortable: false, key: "name", },
+      { title: "Email", sortable: false, key: "email" },
       { title: "Create Date", key: "create_date" },
       { title: "Role", key: "role" },
-      { title: "Status", key: "status"},
+      { title: "Status", key: "status" },
       { title: "Actions", key: "actions", sortable: false },
     ],
     desserts: [],
@@ -224,102 +219,70 @@ export default {
     },
   },
 
- async created() {
+  async created() {
     this.initialize();
-
     try {
-     const response = await axios.get('http://localhost:7770/admin/getsUser',{
-      headers: {
-            Authorization: localStorage.getItem('token'),
-          },
+      const response = await axios.get('http://localhost:7770/admin/getsUser', {
+        headers: {
+          Authorization: localStorage.getItem('token'),
+        },
       });
       this.users = response.data;
       console.log(this.users)
     } catch (error) {
       alert("Error" + error)
     }
-    
+
 
   },
 
   methods: {
     initialize() {
       this.search = '';
-      this.desserts = [
-        {
-          userId: '001',
-          name: "Sarah Moore",
-          email: '643150xxxx@lamduan.mfu.ac.th',
-          create_date: '03/06/2024',
-          role: 'staff',
-          status: 'active',
-        },
-        {
-          userId: '002',
-          name: "Jennifer Blaese",
-          email: '643150xxxx@lamduan.mfu.ac.th',
-          create_date: '02/06/2024',
-          role: 'staff',
-          status: 'active',
-        },
-        {
-          userId: '003',
-          name: "Elizabeth Johnson",
-          email: 'Elizabeth.jo@mfu.ac.th',
-          create_date: '02/06/2024',
-          role: 'staff',
-          status: 'active',
-        },
-        {
-          userId: '004',
-          name: "Nicole Brown",
-          email: 'Nicole Brown@gmail.com',
-          create_date: '03/06/2024',
-          role: 'staff',
-          status: 'active',
-        },
-        {
-          userId: '005',
-          name: "Ashley Williams",
-          email: 'Ashley.wil@mfu.ac.th',
-          create_date: '01/06/2024',
-          role: 'staff',
-          status: 'active',
-        },
-       
-      ];
     },
 
-      generateUserId() {
-      // Generate a unique userId based on current timestamp
-      // const randomNumber = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
-      // return (this.desserts.length + 1).toString().padStart(3, '0') + randomNumber;
-      return (this.desserts.length + 1).toString().padStart(3, '0');
-    },
+    // generateUserId() {
+    //   // Generate a unique userId based on current timestamp
+    //   // const randomNumber = Math.floor(Math.random() * 1000).toString().padStart(3, '0');
+    //   // return (this.desserts.length + 1).toString().padStart(3, '0') + randomNumber;
+    //   return (this.desserts.length + 1).toString().padStart(3, '0');
+    // },
 
-    generateCreateDate() {
-      // Generate a formatted create_date using current date
-      const now = new Date();
-      const day = now.getDate().toString().padStart(2, '0');
-      const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
-      const year = now.getFullYear();
-      return `${day}/${month}/${year}`;
-    },
+    // generateCreateDate() {
+    //   // Generate a formatted create_date using current date
+    //   const now = new Date();
+    //   const day = now.getDate().toString().padStart(2, '0');
+    //   const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+    //   const year = now.getFullYear();
+    //   return `${day}/${month}/${year}`;
+    // },
 
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
 
+
+
+    // ====================================================================================
     deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item);
+      this.editedIndex = this.users.resutl.indexOf(item);
       this.editedItem = Object.assign({}, item);
       this.dialogDelete = true;
     },
 
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1);
+   async deleteItemConfirm() {
+      this.users.resutl.splice(this.editedIndex, 1);
+      console.log(this.editedItem._id)
+      try {
+        const res = await axios.delete(
+          'http://localhost:7770/admin/deleteUser/' + this.editedItem._id,
+          {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            }
+          }
+        ).then( this.snackbardel = true )
+        console.log(res)
+       } catch (error) {
+        alert("Error" + error)
+       }
       this.closeDelete();
     },
 
@@ -338,33 +301,54 @@ export default {
         this.editedIndex = -1;
       });
     },
+// ============================================================================
 
-    save() {
 
-       // Check if userId is present, if not, generate one
-      if (!this.editedItem.userId) {
-        this.editedItem.userId = this.generateUserId();
-      }
 
-      // Check if create_date is present, if not, generate one
-      if (!this.editedItem.create_date) {
-        this.editedItem.create_date = this.generateCreateDate();
-      }
 
-      if (!this.editedItem.email || !/.+@.+\..+/.test(this.editedItem.email)) {
-      alert('Please enter a valid email.');
-      return; // Stop execution if email is not valid
-      }
+    // Save and edited user ========================================================================
 
-      if (!this.editedItem.name || !this.editedItem.email || !this.editedItem.role || !this.editedItem.status) {
-        alert('Please fill in all required fields.');
-        return;
-      }
+    editItem(item) {
+      this.editedIndex = this.users.resutl.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.dialog = true;
+    },
+
+  async  save() {
+
+      // if (!this.editedItem.email || !/.+@.+\..+/.test(this.editedItem.email)) {
+      //   alert('Please enter a valid email.');
+      //   return; // Stop execution if email is not valid
+      // }
+
+      // if (!this.editedItem.name || !this.editedItem.email || !this.editedItem.role || !this.editedItem.status) {
+      //   alert('Please fill in all required fields.');
+      //   return;
+      // }
 
       if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem);
+        Object.assign(this.users.resutl[this.editedIndex], this.editedItem);
+        console.log(this.editedItem._id)
+       try {
+        const res = await axios.patch(
+          'http://localhost:7770/admin/updatePatch/' + this.editedItem._id,
+          {
+            email: this.editedItem.email,
+            role: this.editedItem.role,
+            status: this.editedItem.status
+          },
+          {
+            headers: {
+              Authorization: localStorage.getItem('token')
+            }
+          }
+        ).then(this.snackbaredit = true)
+       
+       } catch (error) {
+        alert("Error" + error)
+       }
       } else {
-        this.desserts.push(this.editedItem);
+        this.users.resutl.push(this.editedItem);
       }
       this.close();
     }
