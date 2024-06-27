1c586c7aa90006c608811fbcea4ce9efe4189eed
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-06-27 22:20:10 +0700

    add function CounterVisitor

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -77,10 +77,8 @@
                       " class="text-xs">
                       {{ item.techReadiness }}
                     </v-chip>
+                    <!-- Views Counter -->
                     <ViewCounter :productId="item._id" />
-                    <!-- <v-chip>
-                      {{ productCounts[item._id] || 0 }} views
-                    </v-chip> -->
                   </v-card-actions>
                 </v-card>
               </router-link>
@@ -101,7 +99,7 @@
 </template>
 
 <script>
-import { defineComponent, ref, } from "vue";
+import { defineComponent, ref, onMounted } from "vue";
 import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
 import ViewCounter from '@/components/ViewCounter.vue'
@@ -137,38 +135,33 @@ export default defineComponent({
   },
 
   // counter visitor ====================================================================
-  // async setup() {
-  //   const globalVisitorCount = ref(0)
-  //   const sessionId = ref(localStorage.getItem('sessionId') || '')
-  //   const productCounts = ref({})
+  async setup() {
+    const globalVisitorCount = ref(0)
+    const sessionId = ref(localStorage.getItem('sessionId') || '')
 
-  //   const generateSessionId = () => {
-  //     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  //   }
+    const generateSessionId = () => {
+      return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+    }
 
-  //   const fetchGlobalCount = async () => {
-  //     if (!sessionId.value) {
-  //       sessionId.value = generateSessionId()
-  //       localStorage.setItem('sessionId', sessionId.value)
-  //     }
-
-  //     try {
-  //       await axios.get(`http://localhost:7770/visitor-count`, {
-  //         params: { sessionId: sessionId.value }
-  //       })
-  //       const response = await axios.get('http://localhost:7770/all-product-counts')
-  //       productCounts.value = response.data.productCounts
-  //     } catch (error) {
-  //       console.error('Error fetching global visitor count:', error)
-  //       alert(error)
-  //     }
-  //   }
-  //   onMounted(fetchGlobalCount)
-  //   return {
-  //     productCounts,
-  //     globalVisitorCount
-  //   }
-  // },
+    const fetchGlobalCount = async () => {
+      if (!sessionId.value) {
+        sessionId.value = generateSessionId()
+        localStorage.setItem('sessionId', sessionId.value)
+      }
+      try {
+        await axios.get(`http://localhost:7770/visitor-count`, {
+          params: { sessionId: sessionId.value }
+        })
+      } catch (error) {
+        console.error('Error fetching global visitor count:', error)
+        alert(error)
+      }
+    }
+    onMounted(fetchGlobalCount)
+    return {
+      globalVisitorCount
+    }
+  },
 
   async created() {
     // Fetch api research and News(Banner) =======================================================================================
