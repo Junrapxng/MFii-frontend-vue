11498cda19e7f84fe89c8a664bf6ad1c4d28ed27
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-02 22:23:01 +0700

    add upload url and del card

--- src/pages/staff/NewsPost.vue
+++ src/pages/staff/NewsPost.vue
@@ -7,7 +7,9 @@
             <v-card-title>สร้างโพสข่าวสาร</v-card-title>
             <v-card-text>
               <v-form @submit.prevent="addNews">
-                <v-text-field v-model="news.url" label="URL" clearable prepend-icon="mdi-web"
+                <v-text-field v-model="news.linkVideo" label="URL video" clearable prepend-icon="mdi-youtube"
+                  variant="solo-filled"></v-text-field>
+                  <v-text-field v-model="news.linkImage" label="URL Images" clearable prepend-icon="mdi-web"
                   variant="solo-filled"></v-text-field>
                 <v-file-input v-model="news.images" label="Upload Images" chips show-size
                   variant="solo-filled"></v-file-input>
@@ -22,8 +24,11 @@
             <v-window-item>
               <v-row>
                 <v-col v-for="(img, index) in imgs" :key="index" cols="12" md="4">
-                  <v-card class="hover:shadow-lg transition-shadow rounded-lg" style="max-width: 400px">
-                    <v-img :src="`http://localhost:7770/${img.filePath}`" height="150px" />
+                  <v-card class="hover:shadow-lg transition-shadow rounded-lg my-1 mx-1" style="max-width: 400px">
+                    <v-img v-if="img.filePath.length > 0" :src="`http://localhost:7770/${img.filePath}`" height="150px" cover />
+                    <v-img v-else-if="img.linkImage.length > 0" :src="`${img.linkImage}`" height="150px" cover />
+                    <iframe v-else-if="img.linkVideo.length > 0" :src="`${img.linkVideo}`" height="150px" class="w-full"></iframe>
+                    <p v-else>No media available</p>
                     <!-- Centered delete button -->
                     <v-container class="d-flex justify-center">
                       <v-btn @click="confirmDelete(img._id, index)" color="error" icon>
@@ -36,6 +41,30 @@
             </v-window-item>
           </v-window>
         </v-container>
+
+        <!-- <v-container fluid style="width: 70%">
+          <v-carousel class="myCarousel" hide-delimiter-background height="400">
+            <v-carousel-item v-for="(item, index) in imgs" :key="index">
+              <v-sheet class="d-flex align-center justify-center" height="100%" elevation="10">
+                <iframe v-if="item.linkVideo.length > 0" 
+                        class="video-iframe w-full h-full" 
+                        :src="`${item.linkVideo}`" 
+                        :title="item.title" 
+                        frameborder="0"
+                        allowfullscreen
+                        ></iframe>
+                <v-img v-else-if="item.filePath.length > 0"
+                      :src="`http://localhost:7770/${item.filePath}`"
+                      :alt="item.title"></v-img>
+                 <v-img v-else-if="item.linkImage.length > 0"
+                      :src="`${item.linkImage}`"
+                      :alt="item.title"></v-img>
+                <p v-else>No media available</p>
+              </v-sheet>
+            </v-carousel-item>
+          </v-carousel>
+        </v-container> -->
+
 
         <!-- Confirmation dialog -->
         <v-dialog v-model="dialog" max-width="400">
@@ -87,7 +116,8 @@ export default {
         color: "success", // Default color
       },
       news: {
-        url: "",
+        linkVideo: [],
+        linkImage: [],
         images: [],
       },
       imgs: [], // Array to hold the fetched images
@@ -98,47 +128,66 @@ export default {
     };
   },
   methods: {
-    async addNews() {
-      try {
-        const formData = new FormData();
-        formData.append('url', this.news.url);
+   // Convert URL Video
+   convertToEmbedUrl(url) {
+    const videoId = url.split('v=')[1];
+    const ampersandPosition = videoId.indexOf('&');
+    if (ampersandPosition !== -1) {
+      return `https://www.youtube.com/embed/${videoId.substring(0, ampersandPosition)}`;
+    }
+    return `https://www.youtube.com/embed/${videoId}`;
+  },
+  async addNews() {
+    try {
+      const formData = new FormData();
 
-        // Append images only if they are not empty
-        this.news.images.forEach((image, index) => {
-          if (image.size > 0) { // Check if file is not empty
-            formData.append(`images[${index}]`, image);
+      // Append images if they are not empty
+      this.news.images.forEach((image, index) => {
+        if (image.size > 0) { // Check if file is not empty
+          formData.append(`images[${index}]`, image);
+        }
+      });
+
+      // Convert video and image links to embed format before appending
+      if (this.news.linkVideo.length > 0) {
+        formData.append('linkVideo', this.convertToEmbedUrl(this.news.linkVideo));
+      }
+
+      if (this.news.linkImage.length > 0) {
+        formData.append('linkImage', this.news.linkImage);
+      }
+
+      // Check if formData has any files or links before making the request
+      if (formData.has('images[0]') || formData.has('linkVideo') || formData.has('linkImage')) {
+        const response = await axios.post('http://localhost:7770/staff/addNews', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: localStorage.getItem("token"),
           }
         });
 
-        // Check if formData has any files before making the request
-        if (formData.has('images[0]')) {
-          const response = await axios.post('http://localhost:7770/staff/addNews', formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-              Authorization: localStorage.getItem("token"),
-            }
-          });
-          console.log(response.data);
-          this.fetchImg(); // Reload images after upload
-          this.news.images = [];
-          this.snackbar.message = "News and images uploaded successfully!";
-          this.snackbar.color = "success";
-          this.snackbar.show = true;
-        } else {
-          alert('No images selected or all selected images are empty.');
-        }
-      } catch (error) {
-        console.error("Error adding News:", error);
-        if (!error.response) {
+        console.log(response.data);
+        this.fetchImg(); // Reload images after upload
+        this.news.images = [];
+        this.news.linkVideo = '';
+        this.news.linkImage = '';
+        this.snackbar.message = "News and images uploaded successfully!";
+        this.snackbar.color = "success";
+        this.snackbar.show = true;
+      } else {
+        alert('No images selected or all selected images are empty.');
+      }
+    } catch (error) {
+      console.error("Error adding News:", error);
+      if (!error.response) {
         this.snackbar.message = "Error adding News: " + error;
       } else {
-        this.snackbar.message = "Error adding News: " + error.response.data.description.description + " Code: " + error.response.status;
+        this.snackbar.message = "Error adding News: " + error.response.data.description + " Code: " + error.response.status;
       }
       this.snackbar.color = "error"; // Set error color
       this.snackbar.show = true;
-
-      }
-    },
+    }
+  },
     async fetchImg() {
       console.log('fetchImg method called.');
       try {
