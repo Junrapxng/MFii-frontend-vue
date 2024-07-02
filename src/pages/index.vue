edee921a107e19d8ebd7df2b3027bd3c25e7e6ee
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-02 22:58:27 +0700

    ดึงแก้ไข video ได้

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,17 +4,19 @@
     <v-main>
       <!-- Carousel Slide -->
       <v-container class="flex">
-        <v-container luid style="width: 90%">
+        <v-container fluid style="width: 90%">
           <Carousel class="carousel" :autoplay="4000" :wrap-around="true">
             <Slide v-for="(path, index) in images" :key="index">
-              <v-img class="carousel__item mx-auto" max-height="500" lazy-src=""
-                :src="`http://localhost:7770/${path.filePath}`" cover>
-                <template v-slot:placeholder>
-                  <div v-if="images" class="d-flex align-center justify-center fill-height">
-                    <v-progress-circular color="pink" indeterminate></v-progress-circular>
-                  </div>
-                </template>
-              </v-img>
+              <template v-if="path.filePath">
+                <v-img class="carousel__item mx-auto" max-height="500" lazy-src=""
+                  :src="`http://localhost:7770/${path.filePath}`" cover>
+                  <template v-slot:placeholder>
+                    <div v-if="images" class="d-flex align-center justify-center fill-height">
+                      <v-progress-circular color="pink" indeterminate></v-progress-circular>
+                    </div>
+                  </template>
+                </v-img>
+              </template>
             </Slide>
             <template #addons>
               <Pagination />
@@ -29,26 +31,23 @@
         <h1 class="text-2xl font-bold mb-3">Success Case</h1>
       </div>
 
-
       <v-container fluid style="width: 70%">
         <v-carousel class="myCarousel" hide-delimiter-background height="400">
-          <v-carousel-item v-for="(video, index) in videos" :key="index">
-            <v-sheet class="d-flex align-center justify-center" height="100%" elevation="10">
-              <iframe class="video-iframe" :src="video.src" :title="video.title" frameborder="0"
-                allowfullscreen></iframe>
+          <v-carousel-item v-for="(video, index) in images" :key="index">
+            <v-sheet  v-if="video.linkVideo && video.linkVideo.length > 0 && video.linkVideo !== null" class="d-flex align-center justify-center" height="100%" elevation="10">
+              <iframe class="video-iframe" :src="video.linkVideo" :title="video.title" frameborder="0" allowfullscreen></iframe>
             </v-sheet>
           </v-carousel-item>
         </v-carousel>
       </v-container>
 
-      
       <!-- Newer research research ใหม่-->
-      
+
       <v-container>
-      <p class="text-3xl font-semibold mb-3">ของใหม่</p>
+        <p class="text-3xl font-semibold mb-3">ของใหม่</p>
         <v-container class="flex justify-center items-center bg-gray-100 rounded">
           <v-row v-if="newinfo" class="flex flex-wrap justify-center">
-            <v-col v-for="(item, index) in newinfo.slice(0,4)" :key="index" cols="12" sm="6" md="6" lg="3" class="p-2">
+            <v-col v-for="(item, index) in newinfo.slice(0, 4)" :key="index" cols="12" sm="6" md="6" lg="3" class="p-2">
               <router-link :to="{ name: 'Innovation', params: { id: item._id } }">
                 <v-card class="hover:shadow-lg transition-shadow rounded-xl" style="max-width: 400px">
                   <v-img :src="`http://localhost:7770/${item.filePath[1]}`" cover height="200px">
@@ -66,12 +65,12 @@
                   }}</v-card-subtitle>
                   <v-card-actions>
                     <v-chip outlined :color="item.techReadiness === 'ระดับการทดลอง'
-                      ? 'purple'
-                      : item.techReadiness === 'ระดับต้นแบบ'
-                        ? 'blue'
-                        : item.techReadiness === 'ระดับถ่ายทอด'
-                          ? 'orange'
-                          : 'default'
+                        ? 'purple'
+                        : item.techReadiness === 'ระดับต้นแบบ'
+                          ? 'blue'
+                          : item.techReadiness === 'ระดับถ่ายทอด'
+                            ? 'orange'
+                            : 'default'
                       " class="text-xs">
                       {{ item.techReadiness }}
                     </v-chip>
@@ -88,8 +87,6 @@
           </v-row>
         </v-container>
       </v-container>
-
-
 
       <!-- Content -->
       <v-container class="inputSearch">
@@ -119,12 +116,12 @@
                   }}</v-card-subtitle>
                   <v-card-actions>
                     <v-chip outlined :color="item.techReadiness === 'ระดับการทดลอง'
-                      ? 'purple'
-                      : item.techReadiness === 'ระดับต้นแบบ'
-                        ? 'blue'
-                        : item.techReadiness === 'ระดับถ่ายทอด'
-                          ? 'orange'
-                          : 'default'
+                        ? 'purple'
+                        : item.techReadiness === 'ระดับต้นแบบ'
+                          ? 'blue'
+                          : item.techReadiness === 'ระดับถ่ายทอด'
+                            ? 'orange'
+                            : 'default'
                       " class="text-xs">
                       {{ item.techReadiness }}
                     </v-chip>
@@ -153,8 +150,6 @@
           </v-btn>
         </template>
       </v-snackbar>
-
-
     </v-main>
     <Footer></Footer>
     <router-view></router-view>
@@ -165,17 +160,17 @@
 import { defineComponent, ref, onMounted } from "vue";
 import { Carousel, Navigation, Slide, Pagination } from "vue3-carousel";
 import "vue3-carousel/dist/carousel.css";
-import ViewCounter from '@/components/ViewCounter.vue'
+import ViewCounter from "@/components/ViewCounter.vue";
 import axios from "axios";
 export default defineComponent({
   name: "index-page",
   data() {
-    return { 
+    return {
       snackbar: {
-      show: false,
-      message: "",
-      color: "success", // Default color
-    },
+        show: false,
+        message: "",
+        color: "success", // Default color
+      },
       search: "",
       userId: ref(null),
       userinfo: ref(null),
@@ -183,52 +178,40 @@ export default defineComponent({
       newinfo: [],
       images: [],
       loading: true,
-      videos: [
-        {
-          src: "https://www.youtube.com/embed/dQw4w9WgXcQ",
-          title: "1",
-        },
-        {
-          src: "https://www.youtube.com/embed/8O1Vr3ro6mM",
-          title: "2",
-        },
-        {
-          src: "https://www.youtube.com/embed/Qe8WHg410t0",
-          title: "2",
-        },
-      ],
       currentPage: 1,
       itemsPerPage: 4,
-      // Define info property here
     };
   },
 
   // counter visitor ====================================================================
   async setup() {
-    const globalVisitorCount = ref(0)
-    const sessionId = ref(localStorage.getItem('sessionId') || '')
+    const globalVisitorCount = ref(0);
+    const sessionId = ref(localStorage.getItem("sessionId") || "");
 
     const generateSessionId = () => {
-      return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    }
+      return (
+        Math.random().toString(36).substring(2, 15) +
+        Math.random().toString(36).substring(2, 15)
+      );
+    };
 
     const fetchGlobalCount = async () => {
       if (!sessionId.value) {
-        sessionId.value = generateSessionId()
-        localStorage.setItem('sessionId', sessionId.value)
+        sessionId.value = generateSessionId();
+        localStorage.setItem("sessionId", sessionId.value);
       }
       try {
         await axios.get(`http://localhost:7770/visitor-count`, {
-          params: { sessionId: sessionId.value }
-        })
+          params: { sessionId: sessionId.value },
+        });
       } catch (error) {
-        console.error('Error fetching global visitor count:', error)
+        console.error("Error fetching global visitor count:", error);
       }
-    }
-    onMounted(fetchGlobalCount)
+    };
+    onMounted(fetchGlobalCount);
     return {
-      globalVisitorCount
-    }
+      globalVisitorCount,
+    };
   },
 
   async created() {
@@ -241,35 +224,36 @@ export default defineComponent({
 
       if (api1Response.status == 200 && api2Response.status == 200) {
         // Filter out the data to get only those with status "active"
-        const activeData = api1Response.data.result.filter(item => item.status === "active");
+        const activeData = api1Response.data.result.filter(
+          (item) => item.status === "active"
+        );
         if (activeData.length > 0) {
           this.info = activeData;
           this.newinfo = [...activeData].reverse(); // Reverse only newinfo
           this.images = api2Response.data.result;
-          console.log("images is " + this.images.result)
+          console.log("images is " + this.images.result);
         } else {
           console.log("No active data found");
-          this.snackbar.message = "No active data found : " + error.message;
+          this.snackbar.message = "No active data found.";
           this.snackbar.color = "error"; // Set error color
           this.snackbar.show = true;
         }
-        
       } else {
-        this.error = new Error("One or both API responses are not OK");
-        console.error(
-          "API responses:",
-          api1Response.status,
-          api2Response.status
-        );
-
+        const errorMessage = `API responses not OK: Research API status: ${api1Response.status}, News API status: ${api2Response.status}`;
+        this.error = new Error(errorMessage);
+        console.error(errorMessage);
       }
     } catch (error) {
       this.error = error;
       console.error("Error fetching data:", error);
-      if(!error.response){
+      if (!error.response) {
         this.snackbar.message = "Error fetching data: " + error;
       } else {
-        this.snackbar.message = "Error fetching data: " + error.response.data.description.description + " Code: " + error.response.status;
+        this.snackbar.message =
+          "Error fetching data: " +
+          error.response.data.description.description +
+          " Code: " +
+          error.response.status;
       }
       this.snackbar.color = "error"; // Set error color
       this.snackbar.show = true;
@@ -279,7 +263,6 @@ export default defineComponent({
     // ==============================================================================================================================
   },
   computed: {
-
     // คำนวณจำนวนหน้าทั้งหมด
     totalPages() {
       return Math.ceil(this.filteredItems.length / this.itemsPerPage);
