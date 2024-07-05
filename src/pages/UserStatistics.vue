5f485f2776a543fea8b7fbb4a5862755210bd6f5
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-05 22:18:41 +0700

    add getViewerCount function

--- src/pages/UserStatistics.vue
+++ src/pages/UserStatistics.vue
@@ -7,19 +7,19 @@
           <v-col>
             <v-card>
               <v-card-title>จำนวนผู้ใช้งานวันนี้</v-card-title>
-              <v-card-text>{{ counter }}</v-card-text>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card>
-              <v-card-title>จำนวนผู้ใช้งานสัปดาห์นี้</v-card-title>
-              <v-card-text>{{ weeklyUsers }}</v-card-text>
+              <v-card-text>{{ todayUsers }}</v-card-text>
             </v-card>
           </v-col>
           <v-col>
             <v-card>
               <v-card-title>จำนวนผู้ใช้งานเดือนนี้</v-card-title>
               <v-card-text>{{ monthlyUsers }}</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col>
+            <v-card>
+              <v-card-title>จำนวนผู้ใช้งานปีนี้</v-card-title>
+              <v-card-text>{{ yearlyUsers }}</v-card-text>
             </v-card>
           </v-col>
           <v-col>
@@ -35,18 +35,39 @@
 </template>
 
 <script>
+import { api } from '../axios';
 
 export default {
   data() {
     return {
       totalUsers: 0,
       todayUsers: 0,
-      weeklyUsers: 0,
       monthlyUsers: 0,
-      
-    }
+      yearlyUsers: 0,
+    };
   },
-}
+  created() {
+    this.getViewerCount();
+  },
+  methods: {
+    async getViewerCount() {
+      try {
+        const response = await api.get('/getStatAll');
+        const viewerCounts = response.data.result;
+
+        // Assuming viewerCounts is the object fetched from MongoDB
+        this.totalUsers = viewerCounts.totalAccess;
+        this.todayUsers = viewerCounts.dailyAccess
+        this.monthlyUsers = viewerCounts.monthlyAccess
+        this.yearlyUsers = viewerCounts.yearlyAccess
+
+        console.log('Viewer counts:', viewerCounts);
+      } catch (error) {
+        console.error('Error fetching viewer counts:', error);
+      }
+    },
+  },
+};
 </script>
 
 <style scoped></style>
