858c9c55b594524200e2b5a14a29d43838ad487f
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-07 05:07:23 +0700

    /api

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:7770',
+  baseURL: 'http://localhost:7770/api',
   withCredentials: true,  // Important for sending cookies
   credentials: 'include'
 });
 
-const url = 'http://localhost:7770';
+const url = 'http://localhost:7770/api';
 
 export { api, url };
