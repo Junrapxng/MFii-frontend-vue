8790b20e919f8531a768880c49bb377fed42e330
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-07 04:58:16 +0700

    use cookieConsent component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,18 @@
     <v-main>
       <index />
       <router-view />
+      <cookie-consent />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
 import { useUserStore } from './stores/user';
+import CookieConsent from './components/CookieConsent.vue';
 const userStore = useUserStore();
 userStore.fetchUser();
+
+
 
 </script>
 
