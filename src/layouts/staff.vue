cae6f04418fd793e171ad2bab60d440016a2338e
NathanB054 <6431501054@lamduan.mfu.ac.th>
2024-07-07 13:11:48 +0700

    fix and add
    fix news bug fix show count show count in all catagories bug and add button to go home

--- src/layouts/staff.vue
+++ src/layouts/staff.vue
@@ -43,7 +43,17 @@
             </v-list-item-icon>
             <v-list-item-title class="inline">หน้าหลัก ADMIN</v-list-item-title>
           </v-list-item>
+
+
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon class="mr-2">mdi-home-floor-a</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title class="inline">Home page</v-list-item-title>
+          </v-list-item>
       </v-list>
+
+      
 
       <!-- logout -->
       <template #append>
