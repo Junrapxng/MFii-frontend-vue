9704323027eb50f9d847d5a14999091e622375a9
Junrapxng <6431501013@lamduan.mfu.ac.th>
2024-07-07 22:51:59 +0700

    use NewsAndVideo Components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,56 +2,11 @@
   <v-app>
     <NavBar />
     <v-main>
-      <!-- Carousel Slide -->
-      <!-- display only images -->
-      <!-- <v-container class="flex"> -->
-        <v-container fluid  class="flex" style="width: 90%">
-          <v-carousel v-if="filteredImages.length" :key="carouselKey" class="Carousel" cycle show-arrows="hover"
-            hide-delimiter-background height="370" :value="0">
-            <v-carousel-item v-for="(path, index) in filteredImages" :key="index">
-              <template v-if="path.linkImage && path.linkImage.length">
-                <v-img v-for="(link, linkIndex) in path.linkImage" :key="`link-${linkIndex}`"
-                  class="carousel__item mx-auto" height="370" lazy-src="" :src="link" contain>
-                  <template v-slot:placeholder>
-                    <div class="d-flex align-center justify-center fill-height">
-                      <v-progress-circular color="pink" indeterminate></v-progress-circular>
-                    </div>
-                  </template>
-                </v-img>
-              </template>
-              <template v-else-if="path.filePath && path.filePath.length">
-                <v-img v-for="(file, fileIndex) in path.filePath" :key="`file-${fileIndex}`"
-                  class="carousel__item mx-auto" height="370" lazy-src="" :src="`${baseUrl}/${file}`" contain>
-                  <template v-slot:placeholder>
-                    <div class="d-flex align-center justify-center fill-height">
-                      <v-progress-circular color="pink" indeterminate></v-progress-circular>
-                    </div>
-                  </template>
-                </v-img>
-              </template>
-            </v-carousel-item>
-          </v-carousel>
-        </v-container>
-      <!-- </v-container> -->
 
-      <!-- Youtube video -->
-      <div class="txt flex justify-center">
-        <h1 class="text-2xl font-bold mb-3">Success Case</h1>
-      </div>
-      <v-container class="youtubeContainer" fluid>
-        <v-carousel v-if="filteredVideos.length > 0" :key="carouselKey" class="myCarousel" hide-delimiter-background
-          height="400" :value="0">
-          <v-carousel-item v-for="(item, index) in filteredVideos" :key="index">
-            <v-sheet class="d-flex align-center justify-center" height="100%" elevation="10">
-              <iframe class="video-iframe" :src="item.linkVideo" :title="item.title || `Video ${index + 1}`"
-                frameborder="0" allowfullscreen></iframe>
-            </v-sheet>
-          </v-carousel-item>
-        </v-carousel>
-      </v-container>
+      <!-- News And Video YouTube -->
+      <NewsAndVideo></NewsAndVideo>
 
       <!-- Newer research research ใหม่-->
-
       <v-container>
         <p class="text-3xl font-semibold mb-3">เพิ่มล่าสุด</p>
         <v-container class="flex justify-center items-center bg-gray-100 rounded">
@@ -68,10 +23,10 @@
                   </v-img>
                   <v-card-title class="text-lg">{{
                     item.nameOnMedia
-                    }}</v-card-title>
+                  }}</v-card-title>
                   <v-card-subtitle class="text-sm">{{
                     item.industryType
-                    }}</v-card-subtitle>
+                  }}</v-card-subtitle>
                   <v-card-actions>
                     <v-chip outlined :color="item.techReadiness === 'ระดับการทดลอง'
                       ? 'purple'
@@ -121,10 +76,10 @@
                   </v-img>
                   <v-card-title class="text-lg">{{
                     item.nameOnMedia
-                    }}</v-card-title>
+                  }}</v-card-title>
                   <v-card-subtitle class="text-sm">{{
                     item.industryType
-                    }}</v-card-subtitle>
+                  }}</v-card-subtitle>
                   <v-card-actions>
                     <v-chip outlined :color="item.techReadiness === 'ระดับการทดลอง'
                       ? 'purple'
@@ -136,8 +91,6 @@
                       " class="text-xs">
                       {{ item.techReadiness }}
                     </v-chip>
-                    <!-- Views Counter -->
-                    <!-- <ViewCounter :productId="item._id" /> -->
                     <v-chip class="mx-2">
                       {{ count[item._id] || 0 }} views
                     </v-chip>
@@ -171,110 +124,80 @@
 </template>
 
 <script>
+import NewsAndVideo from "@/components/NewsAndVideo.vue";
 import { defineComponent, ref } from "vue";
 import { api, url } from "../axios";
 export default defineComponent({
   name: "index-page",
+  components: {
+    NewsAndVideo
+  },
   data() {
     return {
       snackbar: {
         show: false,
         message: "",
-        color: "success", // Default color
+        color: "success",
       },
       search: "",
       userId: ref(null),
       userinfo: ref(null),
       info: [],
       newinfo: [],
-      images: [],
       loading: true,
       currentPage: 1,
       itemsPerPage: 4,
-      carouselKey: 0,
       sessionId: null,
       count: {},
-      baseUrl: "",
+      baseUrl: url,
     };
   },
 
   async created() {
-    this.baseUrl = url;
-    // Fetch api research =======================================================================================
     try {
-      const api1Response = await api.get("/getsResearch/all/all/all/all")
+      const api1Response = await api.get("/getsResearch/all/all/all/all");
 
       if (api1Response.status == 200) {
-        // Filter out the data to get only those with status "active"
-        console.log("test " + api1Response.data);
         this.sessionId = api1Response.data.sessionId;
         const activeData = api1Response.data.result.filter(
           (item) => item.status === "active"
         );
         if (activeData.length > 0) {
           this.info = activeData;
-          this.newinfo = [...activeData].reverse(); // Reverse only newinfo
+          this.newinfo = [...activeData].reverse();
         } else {
-          console.log("No active data found");
           this.snackbar.message = "No active data found.";
-          this.snackbar.color = "error"; // Set error color
+          this.snackbar.color = "error";
           this.snackbar.show = true;
         }
       } else {
-        const errorMessage = `API responses not OK: Research API status: ${api1Response.status},`;
+        const errorMessage = `API responses not OK: Research API status: ${api1Response.status}`;
         this.error = new Error(errorMessage);
         this.snackbar.message = "ERROR: " + errorMessage;
-        this.snackbar.color = "error"; // Set error color
+        this.snackbar.color = "error";
         this.snackbar.show = true;
         console.error(errorMessage);
       }
     } catch (error) {
       this.error = error;
       console.error("Error fetching data:", error);
-      if (!error.response) {
-        this.snackbar.message = "Error fetching data: " + error;
-      } else {
-        this.snackbar.message =
-          "Error fetching data: " +
-          error.response.data.description.description +
-          " Code: " +
-          error.response.status;
-      }
-      this.snackbar.color = "error"; // Set error color
+      this.snackbar.message = "Error fetching data: " + error;
+      this.snackbar.color = "error";
       this.snackbar.show = true;
     } finally {
       this.loading = false;
     }
-
     this.getviewCount();
-    this.Getnews();
-    // ==============================================================================================================================
   },
 
   computed: {
-    // คำนวณจำนวนหน้าทั้งหมด
     totalPages() {
       return Math.ceil(this.info.length / this.itemsPerPage);
     },
-    // กรองรายการโดยใช้การค้นหาและหน้าที่กำหนด
     paginatedItems() {
       const startIndex = (this.currentPage - 1) * this.itemsPerPage;
       const endIndex = startIndex + this.itemsPerPage;
       return this.info.slice(startIndex, endIndex);
-    },
-
-    filteredVideos() {
-      return this.images.filter(
-        (item) => item.linkVideo && item.linkVideo.length > 0
-      );
-    },
-
-    filteredImages() {
-      return this.images.filter(
-        (path) =>
-          (path.linkImage && path.linkImage.length) ||
-          (path.filePath && path.filePath.length)
-      );
     },
   },
   methods: {
@@ -284,9 +207,8 @@ export default defineComponent({
     clearSearch() {
       this.search = "";
       this.fetchResearchData();
-      this.currentPage = "1";
+      this.currentPage = 1;
     },
-    // Search ResearchData
     fetchResearchData() {
       const indust = "all";
       const prop = "all";
@@ -300,7 +222,6 @@ export default defineComponent({
         })
         .then((response) => {
           if (response.status == 200) {
-            // Filter out the data to get only those with status "active"
             const activeData = response.data.result.filter(
               (item) => item.status === "active"
             );
@@ -308,9 +229,8 @@ export default defineComponent({
               this.info = activeData;
             } else {
               this.info = [];
-              console.log("No active data found");
               this.snackbar.message = "No active data found";
-              this.snackbar.color = "error"; // Set error color
+              this.snackbar.color = "error";
               this.snackbar.show = true;
             }
           } else {
@@ -319,57 +239,24 @@ export default defineComponent({
           this.loading = false;
         })
         .catch((error) => {
-          console.error(
-            "There was an error fetching the research data:",
-            error
-          );
-          if (!error.response) {
-            this.snackbar.message = "Error: " + error;
-          } else {
-            this.snackbar.message = "Error geting news" + error.response.description.description + "Code: " + error.response.status;
-          }
-          this.snackbar.color = "error"; // Set error color
+          console.error("There was an error fetching the research data:", error);
+          this.snackbar.message = "Error fetching data: " + error;
+          this.snackbar.color = "error";
           this.snackbar.show = true;
-
-
           this.loading = false;
         });
     },
 
-    // getNews==============================================================================
-
-    async Getnews() {
-      try {
-        const response = await api.get("/getsNews")
-        this.images = response.data.result;
-      } catch (error) {
-        console.log(error);
-        if (!error.response) {
-          this.snackbar.message = "Error geting news" + error;
-        } else {
-          this.snackbar.message = "Error geting news" + error.response.description.description + "Code: " + error.response.status;
-        }
-        this.snackbar.color = "error"; // Set error color
-        this.snackbar.show = true;
-      }
-    },
-
-    // Get view count ===========================================================================
     async getviewCount() {
       try {
-        // Make a single API call to fetch all product counts
         const response = await api.get("/getStatProduct");
         const productCounts = response.data.result;
-
-        // Store product counts in the component's data
         this.count = productCounts;
-
         console.log("Product counts:", this.count);
       } catch (error) {
         console.error("Error fetching product counts:", error);
       }
     },
-    // ============================================================================================
   },
 });
 </script>
